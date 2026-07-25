f190df83db816bc6cd18f74dbf9efd095877b8cd
mind808 <303035700@qq.com>
2026-07-25 14:19:17 +0800

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+﻿const CACHE = "diet-v1"
+const FILES = [".", "index.html", "style.css", "app_all.js", "manifest.json", "icon.svg"]
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())
+  )
+})
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  )
+})
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  )
+})
